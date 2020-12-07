e143fc9e0f8fe9842664bf0263af5439efd5a56e
Yi-An Chen <yachen@nuvoton.com>
2020-12-07 15:30:10 +0800

    Add GD5F1GQ5 & TC58CVG SPI NAND support
    Change-Id: Ib019d8ee7682c9e7de1597b95513d7caffe4678d

--- NuWriterFW_BSP/source/spinandflash.c
+++ NuWriterFW_BSP/source/spinandflash.c
@@ -1098,7 +1098,51 @@ INT spiNAND_ReadINFO(SPINAND_INFO_T *pSN)
             info.SPINand_dummybyte = 0x1;
             info.SPINand_BlockPerFlash = 0x400;// 1024 blocks per 1G NAND
             info.SPINand_PagePerBlock = 64; // 64 pages per block
-        }else {
+        } else if (pSN->SPINand_ID == 0xC851){
+            pSN->SPINand_ID = 0xC851;
+            pSN->SPINand_PageSize=0x800; // 2048 bytes per page
+            pSN->SPINand_SpareArea=64; // 128 bytes per page spare area
+            pSN->SPINand_QuadReadCmd = 0x6b;
+            pSN->SPINand_ReadStatusCmd = 0x0f;
+            pSN->SPINand_WriteStatusCmd =0x1f;
+            pSN->SPINand_StatusValue = 0x1;
+            pSN->SPINand_dummybyte = 0x1;
+            pSN->SPINand_BlockPerFlash = 0x400;// 1024 blocks per 1G NAND
+            pSN->SPINand_PagePerBlock = 64; // 64 pages per block
+
+            info.SPINand_ID = 0xC851;
+            info.SPINand_PageSize=0x800; // 2048 bytes per page
+            info.SPINand_SpareArea=64;    // 128 bytes per page spare area
+            info.SPINand_QuadReadCmd = 0x6b;
+            info.SPINand_ReadStatusCmd = 0x0f;
+            info.SPINand_WriteStatusCmd =0x1f;
+            info.SPINand_StatusValue = 0x1;
+            info.SPINand_dummybyte = 0x1;
+            info.SPINand_BlockPerFlash = 0x400;// 1024 blocks per 1G NAND
+            info.SPINand_PagePerBlock = 64; // 64 pages per block
+        }else if (pSN->SPINand_ID == 0x98E240){
+            pSN->SPINand_ID = 0x98E240;
+            pSN->SPINand_PageSize=0x800; // 2048 bytes per page
+            pSN->SPINand_SpareArea=64; // 128 bytes per page spare area
+            pSN->SPINand_QuadReadCmd = 0x6b;
+            pSN->SPINand_ReadStatusCmd = 0x0f;
+            pSN->SPINand_WriteStatusCmd =0x1f;
+            pSN->SPINand_StatusValue = 0x1;
+            pSN->SPINand_dummybyte = 0x1;
+            pSN->SPINand_BlockPerFlash = 0x400;// 1024 blocks per 1G NAND
+            pSN->SPINand_PagePerBlock = 64; // 64 pages per block
+
+            info.SPINand_ID = 0x98E240;
+            info.SPINand_PageSize=0x800; // 2048 bytes per page
+            info.SPINand_SpareArea=64;    // 128 bytes per page spare area
+            info.SPINand_QuadReadCmd = 0x6b;
+            info.SPINand_ReadStatusCmd = 0x0f;
+            info.SPINand_WriteStatusCmd =0x1f;
+            info.SPINand_StatusValue = 0x1;
+            info.SPINand_dummybyte = 0x1;
+            info.SPINand_BlockPerFlash = 0x400;// 1024 blocks per 1G NAND
+            info.SPINand_PagePerBlock = 64; // 64 pages per block
+        } else {
             printf("SPI NAND ID 0x%x not support!! \n", pSN->SPINand_ID);
             pSN->SPINand_ID = 0x0;//pSN->SPINand_ID;
         }
